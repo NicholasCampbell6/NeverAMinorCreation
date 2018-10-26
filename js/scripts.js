6e36d8dca68231b025cb45813177fe72e9e6f4db
Nic <nicholas.campbell@students.jmcss.org>
2018-10-26 08:48:15 -0500

    10/26/18

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Read more/less button
+
 function readMore(x, y, z) {
   var dots = document.getElementById(x);
   var moreText = document.getElementById(y);
@@ -13,6 +15,8 @@ function readMore(x, y, z) {
     moreText.style.display = "inline";
   }
 }
+
+//Slide Show
 
 let slideIndex = 1;
 showDivs(slideIndex);
@@ -32,9 +36,13 @@ function showDivs(n) {
     slides[slideIndex-1].style.display = "block";
 }
 
+// Arrays for build a bag
+
 let bag = [];
 let ribbon = [];
 let items = [];
+
+//Constructor for items
 
 class item {
   constructor(name, price) {
@@ -43,4 +51,44 @@ class item {
   }
 }
 
-const birthdayPartyBag = new item("Birthday Party Bag", "$0.15");
+
+//Bag Designs
+
+const birthdayPartyBag = new item("Birthday Party Bag", 0.15);
+const camoDrawstringBag = new item("Camouflage Drawstring Bag", 1.25);
+const canvasToteBag = new item("Canvas Tote Bag", 1.00);
+const chevronBag = new item("Chevron Bag", 0.15);
+const confettiBag = new item("Confetti Bag", 0.15);
+const dotsBag = new item("Dots Bag", 0.15);
+const flowerBag = new item("Flower Bag", 0.15);
+const halloweenAssortment = new item("Halloween assortment", 0.15);
+const neonNetBackpackLarge = new item("Neon Net Backpack Large", 1.50);
+const retroBag = new item("Retro Bag", 0.15);
+const sportsBag = new item("Sports Bag", 0.15);
+const sportsDrawstringBagLarge = new item("Sports Drawstring Bag Large", );
+const starsBag = new item("Stars Bag", 0.15);
+
+
+//Ribbons
+
+const noRibbon = new item("No Ribbon", 0.00);
+const orangeRibbon = new item("Orange Ribbon", 0.50);
+const pinkRibbon = new item("Pink Ribbon", 0.50);
+const redRibbon = new item("Red Ribbon", 0.50);
+const yellowRibbon = new item("Yellow Ribbon", 0.50);
+
+//Items
+/*
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+const x = new item("", );
+*/
